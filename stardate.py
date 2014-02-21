8641aa59d80974acaf6bf3c1113d66fc4150957c
Rahul Anand <rahulanand.fine@gmail.com>
2014-02-21 15:19:43 +0530

    Added stardate.py; Skeleton class

---
+++ stardate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# Author: Rahul Anand
+# Description: Stardate conversion of Gregorian calender dates and vice versa
+#              Uses TOS style stardates as adopted by Google Calender 
+# Python version: 2.7
+#####################################################################################
+
+class Stardate():
+    __init__(self):
+        pass
+
+    toStardate(self, date):
+        pass
+
+    fromStardate(self, stardate):
+        pass
